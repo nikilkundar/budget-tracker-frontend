beec5e53ca134afb47c5aaa4c5a7c7c13f98350b
Nikil Kundar <nikilkunder01@gmail.com>
2025-12-17 20:15:12 +0530

    Fix double BrowserRouter and SPA routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+
+      {/* fallback route */}
+      <Route path="*" element={<Login />} />
+    </Routes>
   );
 }
